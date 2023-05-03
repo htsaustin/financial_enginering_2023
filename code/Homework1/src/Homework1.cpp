d1b22ffb6b356230f01399983e7b29b89ff04ca8
Harry Austin <htsaustin@gmail.com>
2023-05-03 18:51:39 +0100

    Theoretically functioning code, upto and including Example 5

--- code/Homework1/src/Homework1.cpp
+++ code/Homework1/src/Homework1.cpp
@@ -1,6 +1,8 @@
 //Include io -> from terminal
 #include <iostream>
 #include <cmath>
+#include "BinModel01.h"
+#include "Options03.h"
 
 using namespace std;
 
@@ -22,5 +24,41 @@ double newtonSymb(int N, int i) {
 int main() {
 
     cout << "newtonSymb(5, 3): "<< newtonSymb(5, 3);
+
+
+    //From slide 12/3
+    // h_call(S) = (S - K)^+, i.e. the 0 or +ve value
+    // ...and...
+    // S(n, i) = S(0)(1+U)^i (1+D)^(n-i)
+    //          --> S(N, i) = S(0)(1+U)^i (1+D)^(N-i)
+    //          --> H(N, i) = h(S(N, i)) = h (S(0)(1+U)^i (1+D)^(N-i))
+
+
+    //putting into slide 34, eqn 19...
+    // q = risk neutral probability
+
+
+
+    // Define the in memory location.
+    // We can use &S0 to point to the address later.
+    double S0, U, D, R;
+
+    //Parse and validate input data
+    if (getInputData(S0, U, D, R) == 1) return 1;
+
+
+    double K; //Strike price
+    int N; //Steps to expiry
+    cout << "Enter call option date: " << endl;
+    getInputData(N, K);
+
+
+    //Compute the risk - neutral probability
+    double q = riskNeutralProbability(U, D, R);
+    cout << "Risk - neutral probability q = " << q << endl;
+
+
+    //TODO - implement here.
+
     return 0;
 }
